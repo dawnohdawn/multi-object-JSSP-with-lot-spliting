7b3d1724f9b2055b762b5a1ac2908c5de136a6bf
dawnohdawn <490852298@qq.com>
2019-03-20 08:43:40 +0800

    20190320存档

--- modifiedGAModels/comparisonsOfAlgorithms.py
+++ modifiedGAModels/comparisonsOfAlgorithms.py
@@ -141,7 +141,7 @@ class comparisonsOfAlgorithms:
             self.oneRunData = pd.DataFrame(columns=self.columnNames)
 
         # 让每个算法都是用相同的random seed
-        sed = 20
+        sed = 9
         np.random.seed(sed)
         print(sed)
         for algorithmInd in range(self.algorithmNum):
